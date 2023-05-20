6f6eab04e1ecf21d7272398cd76ffa3093e44579
condor2 <condor20_05@yahoo.it>
2023-05-20 16:11:29 +0300

    Added Database table

---
+++ romanian_cities.sql
@@ -0,0 +1,24 @@
+-----------------------------------------------------------
+
+--
+-- Database: `Romanian Cities`
+--
+
+-----------------------------------------------------------
+
+--
+-- Table structure for table `oc_city`
+--
+
+DROP TABLE IF EXISTS `oc_city`;
+CREATE TABLE `oc_city` (
+  `city_id` int(11) NOT NULL AUTO_INCREMENT,
+  `zone_id` int(11) NOT NULL,
+  `name` varchar(128) NOT NULL,
+  `postcode` varchar(32) NOT NULL,
+  PRIMARY KEY (`city_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `oc_city`
+--
